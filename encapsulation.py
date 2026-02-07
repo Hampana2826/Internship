fd86d5e5d80157beb0b0119fb9af68753240a447
Hampana2826 <hampanagowda8@gmail.com>
2026-02-07 12:27:19 +0530

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -1,26 +1,25 @@
 class InstagramAccount:
     def __init__(self, username, password):
         self.username = username
-        self.__password = password          # private
-        self.__private_reels = []            # private
-        self.__archived_reels = []           # private
+        self.__password = password          
+        self.__private_reels = []           
+        self.__archived_reels = []          
 
-    # Add private reel
+ 
     def add_private_reel(self, reel):
         self.__private_reels.append(reel)
 
-    # Add archived reel
+  
     def add_archived_reel(self, reel):
         self.__archived_reels.append(reel)
 
-    # Display private reels (follower check)
+
     def show_private_reels(self, is_follower):
         if is_follower:
             print("Private Reels:", self.__private_reels)
         else:
             print("You are not a follower. Access denied.")
 
-    # Display archived reels (password check)
     def show_archived_reels(self, password):
         if password == self.__password:
             print("Archived Reels:", self.__archived_reels)
@@ -36,15 +35,15 @@ class InstagramAccount:
             print("Old password is incorrect.")
 
 
-# ---------------- OBJECT CREATION ----------------
+
 
 account = InstagramAccount("john_doe", "1234")
 
-# Add private reels
+
 account.add_private_reel("Gym Reel")
 account.add_private_reel("Travel Reel")
 
-# Add archived reels
+
 account.add_archived_reel("Old Birthday Reel")
 account.add_archived_reel("College Memories Reel")
 
